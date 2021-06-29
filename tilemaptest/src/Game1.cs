f7fdfc0d33fa75b9e7263a9385b167e591529d69
Lioncat2002 <sankhayan2002@gmail.com>
2021-06-29 21:18:43 +0530

    Added collision system

--- tilemaptest/src/Game1.cs
+++ tilemaptest/src/Game1.cs
@@ -19,7 +19,7 @@ namespace tilemaptest
         private TmxMap map;
         private Texture2D tileset;
         //private Texture2D player;
-        private Animation player;
+        private Player player;
 
        
         private TileMapManager mapRenderer;
@@ -50,12 +50,7 @@ namespace tilemaptest
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            player = new Animation(
-                Content.Load<Texture2D>("Sprite Pack 4\\Sprite Pack 4\\1 - Agent_Mike_Running (32 x 32)"),
-                32,
-                32);
-            map = new TmxMap("Content/map.tmx");
+            map = new TmxMap("Content\\map.tmx");
             tileset = Content.Load<Texture2D>("Cave Tileset\\Cave Tileset\\" + map.Tilesets[0].Name.ToString());
             int tileWidth = map.Tilesets[0].TileWidth;
             int tileHeight = map.Tilesets[0].TileHeight;
@@ -64,15 +59,28 @@ namespace tilemaptest
             collisionObjects = new List<Rectangle>();
             foreach (var o in map.ObjectGroups["Collisions"].Objects)
             {
+                if(o.Name=="")
                 collisionObjects.Add(new Rectangle((int) o.X, (int) o.Y, (int) o.Width, (int) o.Height));
-                if (o.Name != "")
-                {
-                    specialRects.Add(o.Name, new Rectangle((int) o.X, (int) o.Y, (int) o.Width, (int) o.Height));
-                }
+                else
+                specialRects.Add(o.Name, new Rectangle((int) o.X, (int) o.Y, (int) o.Width, (int) o.Height));
+                
                 
             }
 
-           
+            Texture2D[] playerAnimations =
+            {
+                Content.Load<Texture2D>("Sprite Pack 4\\Sprite Pack 4\\1 - Agent_Mike_Idle (32 x 32)"
+                ),
+                Content.Load<Texture2D>("Sprite Pack 4\\Sprite Pack 4\\1 - Agent_Mike_Running (32 x 32)"
+                )
+            };
+
+            player = new Player(
+                specialRects["Start"],
+            2,
+                playerAnimations
+            );
+            map = new TmxMap("Content/map.tmx");
             Console.WriteLine("Start pos:"+specialRects["Start"]);
             Console.WriteLine("End pos:"+specialRects["End"]);
         }
@@ -82,6 +90,26 @@ namespace tilemaptest
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
            
+           var initpos = player.playerPos;
+           player.Update();
+           var fallcheck = new Rectangle(player.playerPos.X,player.playerPos.Y+32,32,2);
+           foreach (var r in collisionObjects)
+           {
+               if (r.Intersects(fallcheck))
+               {
+                   player.isFalling = false;
+                   player.playerPos.Y = initpos.Y;
+               }
+               else
+               {
+                   player.isFalling = true;
+               }
+
+               if (r.Intersects(player.playerPos))
+               {
+                   player.playerPos.X = initpos.X;
+               }
+           }
             // TODO: Add your update logic here
             
             base.Update(gameTime);
@@ -92,7 +120,7 @@ namespace tilemaptest
             GraphicsDevice.Clear(Color.CornflowerBlue);
             mapRenderer.Draw(matrix);
             _spriteBatch.Begin();
-            player.Draw(_spriteBatch,specialRects["Start"],gameTime,100);
+            player.Draw(_spriteBatch,gameTime);
             
             _spriteBatch.End();
             //mapRenderer.Draw();
--- tilemaptest/src/Player.cs
+++ tilemaptest/src/Player.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace tilemaptest
 {
     public class Player
     {
-        public void Draw(SpriteBatch _spriteBatch)
+        public Rectangle playerPos;
+        private int speed,fallspeed;
+        private Animation idleAnim;
+        private Animation runAnim;
+        public bool isFalling = false;
+        private bool isIdle, isRunning,isJumping;
+        
+        public Player(Rectangle startPos,int playerSpeed,Texture2D[] playerTex)
         {
+            playerPos = startPos;
+            speed = playerSpeed;
+            idleAnim = new Animation(playerTex[0],32,32);
+            runAnim = new Animation(playerTex[1], 32, 32);
+            isIdle = true;
+            isRunning = false;
+            fallspeed = 6;
+
+        }
+
+        public void Update()
+        {
+            KeyboardState keyboard = Keyboard.GetState();
             
+            if (isFalling)
+            {
+                fallspeed = 6;
+            }
+            else if (!isFalling)
+            {
+                fallspeed = 0;
+            }
+            
+            playerPos.Y += fallspeed;
+            if (keyboard.IsKeyDown(Keys.A))
+            {
+                playerPos.X -= speed;
+                isIdle = false;
+                isRunning = true;
+            }
+
+           
+           
+            else if (keyboard.IsKeyDown(Keys.D))
+            {
+                playerPos.X += speed;
+                isIdle = false;
+                isRunning = true;
+            }
+            else
+            {
+                isIdle = true;
+                isRunning = false;
+            }
+
+            
+        }
+
+        
+        public void Draw(SpriteBatch _spriteBatch,GameTime gameTime)
+        {
+            if (isIdle)
+            {
+                idleAnim.Draw(_spriteBatch,playerPos,gameTime,100);
+            }
+            else if (isRunning)
+            {
+                runAnim.Draw(_spriteBatch, playerPos, gameTime, 100);
+            }
         }
     }
 }
